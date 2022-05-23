f30ec71a0c56fcb157d0aae06e3c067aa6f2c37d
aselwilliams <aselwilliams22@gmail.com>
2022-05-23 19:19:27 -0400

    winner is defined

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 function App() {
   const [cells, setCells] = useState([...Array(9).keys()])
-  const [flag, setFlag] = useState(false)
+  const [flag, setFlag] = useState(false);
+  const [dataForX, setDataForX] = useState([])
+  const [dataForO, setDataForO] = useState([])
+  const [winner, setWinner] = useState('')
 
   const results=[
     [1,2,3],
@@ -17,18 +20,33 @@ function App() {
     [0,4,8],
     [2,4,6]
   ]
-  
+
   const handleClick=(i)=>{
     let newData=[...cells]
-    flag ? newData[i]='O' : newData[i]='X';
-    // if(!flag){
-    //   newData[i]='X'
-    // } else {
-    //   newData[i]='O'  
-    // }
+    // flag ? newData[i]='O' : newData[i]='X';
+    if(!flag){
+      newData[i]='X';
+      setDataForX([...dataForX, i])
+    } else {
+      newData[i]='O' 
+      setDataForO([...dataForO, i])
+
+    }
     setCells(newData)
     setFlag(!flag)
   }
+
+  useEffect(()=>{
+for(let i of results){
+  let joined=i.join('')
+  if(dataForX.join('')===joined){
+    console.log('X is a winner')
+  } else if (dataForO.join('')===joined){
+    console.log('O is a winner')
+  }
+}
+  },[dataForO, dataForX])
+
   return (
     <div className="container">
       <div className='row'>
@@ -36,10 +54,16 @@ function App() {
         return(
           <div className='col-sm-4 box' onClick={()=>handleClick(ind)}>
             {i}
-            </div>
+          </div>
         )
       })
     }
+
+      </div>
+      <div className='row'>
+        <div className='col'>
+          {winner}
+        </div>
       </div>
     </div>
   );
